f2e8e9cdcd94b6c5c531b11986d9e91a775336e8
Marcin Kieszek <marcin.kieszek@viait.pl>
2026-01-19 15:22:49 +0100

    [IMP] update version to 18.0.1.0.0 in __manifest__.py

--- currency_rate_update_nbp/__manifest__.py
+++ currency_rate_update_nbp/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Currency Rate Update National Bank of Poland",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "category": "Accounting & Finance",
     "summary": "Allows to download currency exchange rates from National bank of Poland",
     "author": "Akretion, Odoo Community Association (OCA)",
